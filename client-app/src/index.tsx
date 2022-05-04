521cacb56be864ad8caa2cb758b5b7f40a41a5d8
Alvin Lam <hk.alvinlam@gmail.com>
2022-05-04 09:20:27 +0100

    End of section 8

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,19 +5,13 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
-
-/* const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-); */
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <StoreContext.Provider value={store}>
-       <App />
+      <BrowserRouter>
+        <App />      
+      </BrowserRouter>
     </StoreContext.Provider>,
   document.getElementById('root')
 );
