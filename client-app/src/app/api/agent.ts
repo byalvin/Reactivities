8b990fd4966e0c17214d70a3ca876c0e9f2e5d49
Alvin Lam <hk.alvinlam@gmail.com>
2022-05-06 16:45:06 +0100

    End of section 13

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -5,6 +5,7 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import { Activity } from '../models/activity';
 import { history } from '../..';
 import { store } from '../stores/store';
+import { User, UserFormValues } from '../models/user';
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
@@ -12,13 +13,17 @@ const sleep = (delay: number) => {
   })
 }
 
-axios.defaults.baseURL = 'http://localhost:5000/api'
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
+axios.interceptors.request.use(config => {
+  const token = store.commonStore.token;
+  if (token) config.headers!.Authorization = `Bearer ${token}`
+  return config;
+})
 
 axios.interceptors.response.use(async response =>{
-
     await sleep(1000);
     return response;
-
 }, (error: AxiosError) => {
   const {data, status, config} = error.response!;
   switch (status) {
@@ -70,8 +75,15 @@ const Activities = {
   delete: (id: string) => axios.delete(`/activities/${id}`)
 }
 
+const Account = {
+  current: () => requests.get<User>('/account'),
+  login: (user: UserFormValues) => requests.post<User>('/account/login', user),
+  register: (user: UserFormValues) => requests.post<User>('/account/register', user)
+}
+
 const agent = {
-  Activities
+  Activities,
+  Account
 }
 
 export default agent;
