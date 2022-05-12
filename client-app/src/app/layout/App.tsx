474d41828c4771c13caa25f8de0bb79b54f64533
Alvin Lam <hk.alvinlam@gmail.com>
2022-05-12 18:06:28 +0100

    End of section 17

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import { act } from 'react-dom/test-utils';
 import { observer } from 'mobx-react-lite';
 import HomePage from '../../features/home/HomePage';
 import { Route, Switch, useLocation } from 'react-router-dom';
@@ -16,6 +15,7 @@ import LoginForm from '../../features/users/LoginForm';
 import { useStore } from '../stores/store';
 import LoadingComponent from './LoadingComponent';
 import ModalContainer from '../common/modals/ModalContainer';
+import ProfilePage from '../../features/profiles/ProfilePage';
 
 function App() {
   const location = useLocation();
@@ -46,6 +46,7 @@ function App() {
                 <Route exact path='/activities' component={ActivityDashboard}/>
                 <Route path='/activities/:id' component={ActivityDetails}/>
                 <Route key={location.key} path={['/createActivity', '/manage/:id']}  component={ActivityForm}/>
+                <Route path='/profiles/:username' component={ProfilePage} />
                 <Route path='/errors' component={TestErrors} />
                 <Route path='/server-error' component={ServerError} />
                 <Route path='/login' component={LoginForm} />
